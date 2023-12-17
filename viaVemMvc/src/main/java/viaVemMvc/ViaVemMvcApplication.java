a5b4235992bb2d408336a0cbd27088a11643becf
David <david.henri.santana@gmail.com>
2023-12-17 16:10:42 -0300

    feat: initial commit

---
+++ viaVemMvc/src/main/java/viaVemMvc/ViaVemMvcApplication.java
@@ -0,0 +1,13 @@
+package viaVemMvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViaVemMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViaVemMvcApplication.class, args);
+	}
+
+}
